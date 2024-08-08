7f21843f56b532eda7182ac345897ce468cb866a
Valter Garcia <valterlanegarcia@gmail.com>
2024-08-08 11:16:48 -0300

    Estilizando e Db

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,10 +4,13 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>{{ $title }} - Controle de Séries</title>
+    <link rel="stylesheet" href="{{ asset('css/app.css')}}">
 </head>
 <body>
+<div class="container">
     <h1>{{ $title }}</h1>
 
     {{ $slot }}  
+</div>
 </body>
 </html>
